7f7fcc9f7d2ecdefd69f94ce20997a4eaecff789
Rupesh Satti <rupesh.satti555@gmail.com>
2026-02-04 12:17:24 +0530

    added_blog_and_search

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render
 
+from others.models import About
 from blogs.models import Category, Blog
 
 
@@ -8,6 +9,9 @@ def home(request):
     categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('created_at')
     posts = Blog.objects.filter(is_featured=False, status='Published').order_by('created_at')
-    
-    context = {'categories': categories, 'featured_posts': featured_posts, 'posts': posts}
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+    context = {'categories': categories, 'featured_posts': featured_posts, 'posts': posts, 'about': about}
     return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from requests import post
 from .models import Blog, Category
+from django.db.models import Q
 
 # Create your views here.
 def posts_by_category(request, category_id):
@@ -20,3 +21,15 @@ def posts_by_category(request, category_id):
     }
     
     return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    context = {'single_blog': single_blog}
+    return render(request, 'blogs.html', context)
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status='Published').order_by('created_at')
+    print(blogs)
+    context = {'blogs': blogs, 'keyword': keyword}
+    return render(request, 'search.html', context)
